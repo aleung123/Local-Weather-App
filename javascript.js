cac40beeba90d38ce927b1c34b65fbc0da460183
Angela Leung <angelaleung@Angelas-MBP-3.fios-router.home>
2018-01-10 22:25:39 -0500

    added javascript file

---
+++ javascript.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+	// get lattitude and longtitude from IP geolocator
+	$.getJSON('http://ip-api.com/json', function(data2) {
+		var lat = data2.lat;
+		var long = data2.lon;
+		// API url with geolocation
+		var api = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=d638f9cae2887d5bf1141babe634e6fd';
+		$.getJSON(api, function(data) {
+			var weatherType = data.weather[0].description;
+			var kelvin = data.main.temp;
+			var windSpeed = data.wind.speed;
+			var city = data.name;
+
+			var fahren = (kelvin*(9/5)-459.67).toFixed(2);
+			var celcius = (kelvin-273).toFixed(2);
+			var tempSwap = true;
+
+			windSpeed = (2.237*windSpeed).toFixed(1);
+
+			$("#city").html(city);
+			$("#weatherType").html(weatherType);
+			$("#fahren").html(fahren + " &#8457;");
+			$("#fahren").click(function() {
+				if (tempSwap === true) {
+					$("#fahren").html(celcius + " &#8451");
+					tempSwap = false;
+				} else {
+					$("#fahren").html(fahren + " &#8457");
+					tempSwap = true;
+				}
+			});
+			$("#windSpeed").html(windSpeed + " mph");
+
+			if (fahren > 80) {
+				$("body").css('background-image', 'url(https://i.ytimg.com/vi/3EXe5cx5S-0/maxresdefault.jpg)');
+			} else if (fahren > 60) {
+				$("body").css('background-image', 'url(https://cdn.pixabay.com/photo/2013/11/28/09/57/sky-219769_1280.jpg)');
+			} else if (fahren > 40) {
+				$("body").css('background-image', 'url(https://ak1.picdn.net/shutterstock/videos/20608981/thumb/1.jpg)');
+			} else {
+				$("body").css('background-image', 'url(http://cdn.pcwallart.com/images/cold-wallpaper-3.jpg)');
+			}
+		});
+	});	
+});
